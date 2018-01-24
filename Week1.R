16a00f28769b0c836476a0e07ec42c0bb9d99063
cbboland <35773750+cbboland@users.noreply.github.com>
2018-01-24 17:00:43 +0000

    Update Week1.R

--- Week1.R
+++ Week1.R
@@ -30,6 +30,21 @@
   iris3 %>% dim()
   history(60)
 
+
+
+
+  library(dplyr)
+  iris %>% group_by(Species) %>% summarize(meanSL = mean(Sepal.Length))
+  iris %>% group_by(Species) %>% summarize(varSL = var(Sepal.Length))
+  iris %>% group_by(Species) %>% summarize(sdSL = sd(Sepal.Length))
+  iris %>% group_by(Species) %>% summarize(medianSL = median(Sepal.Length))
+  iris %>% group_by(Species) %>% summarize(meanSL = mean(Sepal.Length),      varSL = var(Sepal.Length),      sdSL = sd(Sepal.Length),      medianSL = median(Sepal.Length))
+
+  mtcars %>% group_by(cyl) %>% summarize(meanMPG = mean(mpg))
+  mtcars %>% group_by(vs,am) %>% summarize(medianMPG = median(mpg))
+
+  mtcars %>% mutate(sqrtMPG = sqrt(mpg), logMPG = log(mpg)) %>%  select(mpg,sqrtMPG,logMPG,everything() )  %>%  head(5)
+
   
   
   
